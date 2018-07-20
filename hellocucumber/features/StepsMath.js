18ac2db453478f912dcc1134cb23bb9d68051f14
Cesar Aguilar <Cesar.Y.Aguilar.Herrera.-ND@disney.com>
2018-07-20 11:40:03 -0500

    add hello cucumber folder

---
+++ hellocucumber/features/StepsMath.js
@@ -0,0 +1,15 @@
+const assert = require('assert')
+const difineSupportCode = require ('cucumber').defineSupportCode;
+
+
+difineSupportCode(Function({ Given, When, Then}) {
+
+    let answer = 0;
+
+
+
+
+
+
+}); 
+
